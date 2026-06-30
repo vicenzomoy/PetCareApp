2aea97a1c20a4546e674100c40a1f255631cc884
Vicenzo <vicenzomoy@gmail.com>
2026-06-30 12:26:18 +0700

    Revert "setup realm"
    This reverts commit 9cc65e4293e36ee382eb4761c3de3184c09b7222.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
 }
-apply(plugin = "realm-android")
 
 android {
     namespace = "edu.uph.m24si2.petcareapp"
@@ -23,19 +22,14 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            optimization {
+                enable = false
+            }
         }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-    }
-    buildFeatures {
-        viewBinding = true
     }
 }
 
@@ -44,7 +38,6 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.constraintlayout)
     implementation(libs.material)
-    implementation("io.realm:realm-android-library:10.15.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(libs.ext.junit)
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,3 @@
 plugins {
     alias(libs.plugins.android.application) apply false
 }
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:7.4.2")
-        classpath("io.realm:realm-gradle-plugin:10.15.1")
-    }
-}
